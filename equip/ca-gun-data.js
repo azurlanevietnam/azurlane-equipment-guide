bfd2f6b7b412fd52e4dc5892494633a951b39454
Trung <nguyenbaotrung11399@gmail.com>
2026-05-28 20:05:16 +0700

    none

--- equip/ca-gun-data.js
+++ equip/ca-gun-data.js
@@ -8,7 +8,7 @@ equipData["CA-gun"] = {
         property: "gunType",
         fallbackItemValue: "ca",
         buttons: [
-            { value: "all", label: "Danh sách tổng" },
+            { value: "all", label: "Danh Sách Tổng" },
             { value: "ca", label: "Chỉ CA-Gun" },
             { value: "cb", label: "Chỉ CB-Gun" }
         ]
@@ -57,5 +57,47 @@ equipDetails["CA-gun"] = {
         desc: [""],
         code: "c/c2/14000",
         box: "rainbow"
+    },
+    "triple-234": {
+        name: "Triple 234mm (BL 9.2\" Mk XII Prototype)",
+        gunType: "ca",
+        tier: "S+",
+        source: ["PR3 Research", "Prototype Shop"],
+        stats: ["FP +65"],
+        ammoType: "NormalDR",
+        ammoMod: "115% / 115% / 95%",
+        rld: ["7.85s", "Volley Time: 0.60s"],
+        linkTab: 0,
+        desc: [""],
+        code: "8/8f/23120",
+        box: "rainbow"
+    },
+    "triple-203-55": {
+        name: "Prototype Triple 203mm/55 Main Gun Mount",
+        gunType: "ca",
+        tier: "S+",
+        source: ["Effulgence Before Eclipse Event Shop"],
+        stats: ["FP +65"],
+        ammoType: "HE",
+        ammoMod: "140% / 110% / 70%",
+        rld: ["7.60s", "Volley Time: 0.50s"],
+        linkTab: 0,
+        desc: [""],
+        code: "f/ff/33120",
+        box: "rainbow"
+    },
+    "twin-203-skci": {
+        name: "Twin 203mm SKC (Improved)",
+        gunType: "ca",
+        tier: "S+",
+        source: ["Operation Convergence Event Shop"],
+        stats: ["FP +45"],
+        ammoType: "AP",
+        ammoMod: "80% / 115% / 75%",
+        rld: ["7.65s", "Volley Time: 1.00s"],
+        linkTab: 0,
+        desc: [""],
+        code: "c/c1/43080",
+        box: "yellow"
     }
 };
