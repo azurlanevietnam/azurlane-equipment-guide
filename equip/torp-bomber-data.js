931fd168a7e6d99a89f540af1356925fbef274db
Trung <nguyenbaotrung11399@gmail.com>
2026-06-28 14:18:01 +0700

    none

---
+++ equip/torp-bomber-data.js
@@ -0,0 +1,42 @@
+equipData["Torp-bomber"] = {
+    subCategories: [
+        { id: "tb-light", name: "Giáp Light đơn" }
+    ],
+    tierlists: {
+    }
+};
+
+equipDetails["Torp-bomber"] = {
+    "single-152-p1892": {
+        name: "PSingle 152mm (6\"/45 Pattern 1892)",
+        tier: "EX",
+        source: ["3-2 Drop", "Gear Lab"],
+        stats: ["FP +12"],
+        ammoType: "Normal",
+        ammoMod: "100% / 75% / 40%",
+        rld: ["2.43s", "Volley Time: 0.40s"],
+        dmg: ["1", "100"],
+        range: "50",
+        coef: 1.00,
+        linkTab: 3,
+        desc: [""],
+        code: "c/c0/7000",
+        box: "blue"
+    },
+    "quad-152": {
+        name: "Prototype Quadruple 152mm Main Gun Mount",
+        tier: "SS-",
+        source: ["PR5 Research", "Gear Lab", "Prototype Shop"],
+        stats: ["FP +65"],
+        ammoType: "AP+",
+        ammoMod: "110% / 90% / 70%",
+        rld: ["3.10s", "Volley Time: 0.00s"],
+        dmg: ["1", "100"],
+        range: "50",
+        coef: 1.00,
+        linkTab: 0,
+        desc: [""],
+        code: "3/37/22280",
+        box: "rainbow"
+    }
+};
